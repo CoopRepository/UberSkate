5038b9d7ce7e5e2ad051ee97979f145f20ea0fc3
CoopRepository <brendan.kellam@greenwoodcollege.com>
2015-02-24 03:39:24 +0000

    Made many more changes regarding the posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :posts
+  
 
-  #index route: /map
-  get 'map', to: 'post#index'  
-  
-  #show route: /post/:id
-  get 'post/:id', to: 'post#show'
-  
-  #new route: /post/new
-  get 'post/new'
-  
-  #edit route: /post/:id/edit
-  get 'post/:id/edit', to: 'post#edit'
-
-  #delete route: /post/:id/delete
-  get 'post/delete/:id', to: 'post#delete'
-  
   #default root set to index. 
-  root 'post#index'
+  root 'posts#index'
+  get 'posts/:id/delete' => 'posts#delete', :as => :products_delete
 
 end
